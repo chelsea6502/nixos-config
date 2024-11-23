3e28296d1a5955b545f9cbc9c342758414f97c0a
chelsea6502 <mail@chelseawilkinson.me>
2024-11-23 20:15:11 +1100

    initial commit

---
+++ configuration.nix
@@ -0,0 +1,83 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ./cachix.nix
+      <home-manager/nixos>
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  programs.sway.enable = true;
+  services.displayManager.defaultSession = "sway";
+
+  # Set your time zone.
+  time.timeZone = "Australia/Melbourne";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_AU.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_AU.UTF-8";
+    LC_IDENTIFICATION = "en_AU.UTF-8";
+    LC_MEASUREMENT = "en_AU.UTF-8";
+    LC_MONETARY = "en_AU.UTF-8";
+    LC_NAME = "en_AU.UTF-8";
+    LC_NUMERIC = "en_AU.UTF-8";
+    LC_PAPER = "en_AU.UTF-8";
+    LC_TELEPHONE = "en_AU.UTF-8";
+    LC_TIME = "en_AU.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "au";
+    variant = "";
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.chelsea = {
+    isNormalUser = true;
+    description = "chelsea";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      qutebrowser
+      alacritty
+      neovim
+      git      
+    ];
+  };
+
+  services.getty.autologinUser = "chelsea";
+
+  environment.systemPackages = with pkgs; [
+
+  ];
+
+  services.spice-vdagentd.enable = true;
+
+  networking.firewall.enable = false;
+
+
+  home-manager.users.chelsea = {
+    /* The home.stateVersion option does not have a default and must be set */
+    home.stateVersion = "24.05";
+    /* Here goes the rest of your home-manager config, e.g. home.packages = [ pkgs.foo ]; */
+  };
+
+  system.stateVersion = "24.05";
+
+}
