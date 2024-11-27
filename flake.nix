85db918ac16568464d6ab8852f139613c9361558
Chelsea Wilkinson <mail@chelseawilkinson.me>
2024-11-27 19:30:02 +1100

    formatted

--- flake.nix
+++ flake.nix
@@ -15,38 +15,38 @@
         modules = [
           ./configuration.nix
           home-manager.nixosModules.home-manager
-	stylix.nixosModules.stylix
+          stylix.nixosModules.stylix
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-	    home-manager.backupFileExtension = "backup";
+            home-manager.backupFileExtension = "backup";
             home-manager.users.chelsea = {
-	    	home.username = "chelsea";
-		home.homeDirectory = "/home/chelsea";
-		home.stateVersion = "24.05";
-		programs.home-manager.enable = true;
-		programs.qutebrowser.enable = true;
-		programs.foot.enable = true;
-		programs.alacritty.enable = true;
+              home.username = "chelsea";
+              home.homeDirectory = "/home/chelsea";
+              home.stateVersion = "24.05";
+              programs.home-manager.enable = true;
+              programs.qutebrowser.enable = true;
+              programs.foot.enable = true;
+              programs.alacritty.enable = true;
 
-		wayland.windowManager.sway.config = {
-			modifier = "Mod4";
-			output = {
-		  	HDMI-A-1 = {
-		  		resolution = "1920x1080";
-		  	};
-		};
-		};
+              wayland.windowManager.sway.config = {
+                modifier = "Mod4";
+                output = {
+                  HDMI-A-1 = {
+                    resolution = "1920x1080";
+                  };
+                };
+              };
 
 
-		wayland.windowManager.sway.enable = true;
+              wayland.windowManager.sway.enable = true;
 
-				stylix.autoEnable = true;
-	    };
-	   
-		security.polkit.enable = true;
-						stylix.enable = true;
-						stylix.image = ./wallpaper.png;
+              stylix.autoEnable = true;
+            };
+
+            security.polkit.enable = true;
+            stylix.enable = true;
+            stylix.image = ./wallpaper.png;
             # Optionally, use home-manager.extraSpecialArgs to pass
             # arguments to home.nix
           }
