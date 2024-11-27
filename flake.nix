5e6916a21260653234f9f4244acd1f5e79744ea3
Chelsea Wilkinson <mail@chelseawilkinson.me>
2024-11-27 19:04:11 +1100

    working stylix

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-		stylix.url = "github:danth/stylix";
+    stylix.url = "github:danth/stylix/release-24.05";
   };
 
   outputs = inputs@{ nixpkgs, home-manager, stylix, ... }: {
@@ -15,12 +15,19 @@
         modules = [
           ./configuration.nix
           home-manager.nixosModules.home-manager
-					stylix.nixosModules.stylix
+	stylix.nixosModules.stylix
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.chelsea = import ./home.nix;
-
+            home-manager.users.chelsea = {
+	    	home.username = "chelsea";
+		home.homeDirectory = "/home/chelsea";
+		home.stateVersion = "24.05";
+		programs.home-manager.enable = true;
+		programs.qutebrowser.enable = true;
+		programs.foot.enable = true;
+		programs.alacritty.enable = true;
+	    };
 						stylix.enable = true;
 						stylix.image = ./wallpaper.png;
             # Optionally, use home-manager.extraSpecialArgs to pass
