f1aeaf0370892795d18399a59eefd620cfd63744
Chelsea Wilkinson <mail@chelseawilkinson.me>
2024-12-04 20:12:33 +1100

    nixvim config

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -1,25 +1,29 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+  imports =
+    [
+      (modulesPath + "/installer/scan/not-detected.nix")
+    ];
 
   boot.initrd.availableKernelModules = [ "usbhid" "usb_storage" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
-  boot.kernelPackages = (import (builtins.fetchTarball https://gitlab.com/vriska/nix-rpi5/-/archive/main.tar.gz)).legacyPackages.aarch64-linux.linuxPackages_rpi5;
   boot.extraModulePackages = [ ];
 
-  boot.kernelParams = [ "8250.nr_uarts=11" "console=ttyAMA10,9600" "console=tty0" ];
+  boot.kernelParams = [ "8250.nr_uarts=11" "console=ttyAMA10,9600" "console=tty0"];
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/7707dabf-619f-45a5-9407-f5f29855cf44";
-    fsType = "ext4";
-  };
+  fileSystems."/" =
+    {
+      device = "/dev/disk/by-uuid/7707dabf-619f-45a5-9407-f5f29855cf44";
+      fsType = "ext4";
+    };
 
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/7B88-B085";
-    fsType = "vfat";
-  };
+  fileSystems."/boot" =
+    {
+      device = "/dev/disk/by-uuid/7B88-B085";
+      fsType = "vfat";
+    };
 
   swapDevices = [ ];
 
