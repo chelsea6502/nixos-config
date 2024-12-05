ad0a26e6d48da2e28a83d1a733e54665ded7418a
Chelsea Wilkinson <mail@chelseawilkinson.me>
2024-12-05 21:13:07 +1100

    stuff into configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 let
   patchedDwl = (pkgs.dwl.overrideAttrs (old: rec {
     buildInputs = old.buildInputs ++ [ pkgs.fcft pkgs.pixman pkgs.libdrm ];
@@ -28,6 +28,56 @@ in {
   networking.networkmanager.enable = true;
   networking.firewall.enable = true;
 
+  time.timeZone = "Australia/Melbourne";
+  i18n.defaultLocale = "en_AU.UTF-8";
+
+  services.getty.autologinUser = "chelsea";
+  services.openssh.enable = true;
+
+  # sound
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  home-manager.useGlobalPkgs = true;
+  home-manager.useUserPackages = true;
+
+  programs.nixvim = ./nixvim.nix;
+  home-manager.backupFileExtension = "backup";
+  home-manager.users.chelsea = {
+    home.username = "chelsea";
+    home.homeDirectory = "/home/chelsea";
+    home.stateVersion = "24.05";
+    programs.home-manager.enable = true;
+    programs.qutebrowser.enable = true;
+    programs.foot.enable = true;
+
+    programs.git = {
+      enable = true;
+      userName = "Chelsea Wilkinson";
+      userEmail = "mail@chelseawilkinson.me";
+    };
+
+    programs.qutebrowser.settings = {
+      tabs.show = "multiple";
+      statusbar.show = "in-mode";
+      scrolling.smooth = true;
+      content.javascript.clipboard = "access";
+    };
+
+    programs.foot.settings = { main.pad = "24x24 center"; };
+
+    stylix.autoEnable = true;
+  };
+
+  security.polkit.enable = true;
+  stylix.enable = true;
+  stylix.image = ./dwl/wallpaper.jpg;
+
   stylix.base16Scheme =
     "${pkgs.base16-schemes}/share/themes/gruvbox-material-dark-medium.yaml";
 
@@ -50,9 +100,6 @@ in {
     };
   };
 
-  time.timeZone = "Australia/Melbourne";
-  i18n.defaultLocale = "en_AU.UTF-8";
-
   environment.systemPackages = with pkgs; [ git ];
   users.users.chelsea = {
     isNormalUser = true;
@@ -68,9 +115,6 @@ in {
     ];
   };
 
-  services.getty.autologinUser = "chelsea";
-  services.openssh.enable = true;
-
   services.greetd = {
     enable = true;
     settings = rec {
@@ -80,15 +124,6 @@ in {
       };
       default_session = initial_session;
     };
-  };
-
-  # sound
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
   };
 
   system.stateVersion = "24.05";
