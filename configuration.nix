7881039c20c39bafe5ec63ab0e3c91808d5b1109
Chelsea Wilkinson <mail@chelseawilkinson.me>
2024-12-05 20:54:20 +1100

    reorg

--- configuration.nix
+++ configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 let
   patchedDwl = (pkgs.dwl.overrideAttrs (old: rec {
@@ -25,14 +21,12 @@ in {
   nix.gc.automatic = true;
   nix.gc.options = "--delete-older-than 7d";
 
-  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos"; # Define your hostname.
-
-  # Enable networking
   networking.networkmanager.enable = true;
+  networking.firewall.enable = true;
 
   stylix.base16Scheme =
     "${pkgs.base16-schemes}/share/themes/gruvbox-material-dark-medium.yaml";
@@ -59,6 +53,7 @@ in {
   time.timeZone = "Australia/Melbourne";
   i18n.defaultLocale = "en_AU.UTF-8";
 
+  environment.systemPackages = with pkgs; [ git ];
   users.users.chelsea = {
     isNormalUser = true;
     description = "chelsea";
@@ -73,76 +68,9 @@ in {
     ];
   };
 
-  programs.nixvim.extraPlugins = with pkgs; [
-    vimPlugins.no-neck-pain-nvim
-    vimPlugins.gruvbox-material
-  ];
-
-  programs.nixvim.extraConfigLua = ''
-    	      vim.cmd('colorscheme gruvbox-material')
-        		require('no-neck-pain').setup({
-        				autocmds = { enableOnVimEnter = true, skipEnteringNoNeckPainBuffer = true },
-        				options = { width = 100, minSideBufferWidth = 100 },
-        				buffers = { right = { enabled = false }, wo = { fillchars = 'vert: ,eob: ' }
-        			},
-        			})
-            		
-            local luasnip = require("luasnip")
-            local cmp = require("cmp")
-
-            cmp.setup({
-
-              -- ... Your other configuration ...
-
-              mapping = {
-
-                -- ... Your other mappings ...
-               ['<CR>'] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        if luasnip.expandable() then
-                            luasnip.expand()
-                        else
-                            cmp.confirm({
-                                select = true,
-                            })
-                        end
-                    else
-                        fallback()
-                    end
-                end),
-
-                ["<Tab>"] = cmp.mapping(function(fallback)
-                  if cmp.visible() then
-                    cmp.select_next_item()
-                  elseif luasnip.locally_jumpable(1) then
-                    luasnip.jump(1)
-                  else
-                    fallback()
-                  end
-                end, { "i", "s" }),
-
-                ["<S-Tab>"] = cmp.mapping(function(fallback)
-                  if cmp.visible() then
-                    cmp.select_prev_item()
-                  elseif luasnip.locally_jumpable(-1) then
-                    luasnip.jump(-1)
-                  else
-                    fallback()
-                  end
-                end, { "i", "s" }),
-              },
-            })
-            	'';
-
   services.getty.autologinUser = "chelsea";
-
-  environment.systemPackages = with pkgs; [ git ];
-
-  networking.firewall.enable = true;
-
   services.openssh.enable = true;
 
-  system.stateVersion = "24.05";
   services.greetd = {
     enable = true;
     settings = rec {
@@ -163,4 +91,5 @@ in {
     pulse.enable = true;
   };
 
+  system.stateVersion = "24.05";
 }
