8f8e3f010ba07a1c11e8439b9399837eb6810093
Chelsea Wilkinson <122856043+chelsea6502@users.noreply.github.com>
2024-12-23 17:54:23 +1000

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -213,24 +213,12 @@ in {
     hideMounts = true;
     directories = [
       "/etc/nixos"
-      #"/var/log"
       "/var/lib/nixos"
-      "/var/lib/systemd/coredump"
       "/etc/NetworkManager/system-connections"
-      #{
-      #  directory = "/var/lib/colord";
-      #  user = "colord";
-      #  group = "colord";
-      #  mode = "u=rwx,g=rx,o=";
-      #}
     ];
-    #files = [
-    #  "/etc/machine-id"
-    #  {
-    #    file = "/var/keys/secret_file";
-    #    parentDirectory = { mode = "u=rwx,g=,o="; };
-    #  }
-    #];
+    files = [
+      "/etc/shadow"
+    ];
     users.chelsea = {
       directories = [
         "nixos-config"
@@ -244,16 +232,11 @@ in {
           mode = "0700";
         }
         {
-          directory = ".nixops";
-          mode = "0700";
-        }
-        {
           directory = ".local/share/keyrings";
           mode = "0700";
         }
         ".local/share/direnv"
       ];
-      # files = [ ".screenrc" ];
     };
   };
 
