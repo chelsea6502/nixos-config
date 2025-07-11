30d7f98d13b59fa39033fbf3b41ec6afca678212
Chelsea Wilkinson <122856043+chelsea6502@users.noreply.github.com>
2025-07-11 17:42:22 +1000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
         specialArgs = { inherit inputs; };
         modules = [
           inputs.disko.nixosModules.default
-          (import ./disko.nix { device = "/dev/nvme1"; })
+          (import ./disko.nix { device = "/dev/sda"; })
 
           ./configuration.nix
           inputs.home-manager.nixosModules.home-manager
