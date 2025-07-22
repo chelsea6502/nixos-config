c781db3b962ba8169cee26a164897b7e4ab35e7f
Chelsea Wilkinson <mail@chelseawilkinson.me>
2025-07-22 12:17:55 +1000

    x86 is the default

--- overrides/mac.nix
+++ overrides/mac.nix
@@ -14,5 +14,7 @@
   programs.bash.shellAliases = {
     desk = "wlr-randr --output Virtual-1 --custom-mode 3840x2160 --scale 2";
     lap = "wlr-randr --output Virtual-1 --mode 2560x1600 --scale 2";
+    switch = lib.mkForce "sudo nixos-rebuild switch --flake
+		/etc/nixos/#nixos-mac";
   };
 }
