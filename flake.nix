b4eadfa53af608fd4d68e95698e08eb738d3b108
Chelsea Wilkinson <mail@chelseawilkinson.me>
2025-07-22 12:33:25 +1000

    x86 as default

--- flake.nix
+++ flake.nix
@@ -52,11 +52,8 @@
     in
     {
       nixosConfigurations = {
-        # Default configuration (Parallels/aarch64-linux)
-        nixos = mkSystem "aarch64-linux" ./overrides/mac.nix;
-
-        # x86_64-linux configuration (main PC)
-        nixos-x86 = mkSystem "x86_64-linux" ./overrides/pc.nix;
+        nixos = mkSystem "x86_64-linux" ./overrides/pc.nix;
+        nixos-mac = mkSystem "aarch64-linux" ./overrides/mac.nix;
       };
     };
 }
