0bd3ee936994941b647efd2c64b293951528e575
Chelsea Wilkinson <mail@chelseawilkinson.me>
2025-10-11 16:16:51 +1100

    cleanup + python cleanup

--- configuration.nix
+++ configuration.nix
@@ -22,62 +22,61 @@
     wifi-security.psk = "milawa78";
   };
 
-  boot.loader.systemd-boot.enable = true;
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
 
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  security.pam.services.login.u2fAuth = true;
-  security.pam.services.sudo.u2fAuth = true;
+  security.pam.services = {
+    login.u2fAuth = true;
+    sudo.u2fAuth = true;
+    swaylock = {};
+  };
   services.udev.packages = [ pkgs.yubikey-personalization ];
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   boot.kernelParams = [ "video=3840x2160@240" ];
   hardware.display.outputs.DP-3.mode = "3840x2160@240";
-  nix.settings.max-jobs = "auto";
 
   # SOPS configuration
   sops.defaultSopsFile = ./keys/secrets.yaml;
   sops.defaultSopsFormat = "yaml";
   sops.age.keyFile = "/home/chelsea/.config/sops/age/keys.txt";
   sops.age.generateKey = true;
-  
-  # Ensure the GitHub token file is readable by nix daemon
-  nix.settings.trusted-users = [ "root" "@wheel" ];
 
   boot.initrd.systemd.network.wait-online.enable = false;
   networking.dhcpcd.wait = "background";
 
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
-  
-  # Binary caches for faster builds
-  nix.settings.substituters = [
-    "https://cache.nixos.org"
-    "https://nix-community.cachix.org"
-    "https://nixpkgs-wayland.cachix.org"
-  ];
-  nix.settings.trusted-public-keys = [
-    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
-  ];
-  
-  # Automatic store optimization - hard-links identical files
-  nix.optimise.automatic = true;
-  
-  # Automatic garbage collection
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 7d";
+  nix = {
+    settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+      max-jobs = "auto";
+      trusted-users = [ "root" "@wheel" ];
+      substituters = [
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+        "https://nixpkgs-wayland.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+      ];
+    };
+    optimise.automatic = true;
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
   };
 
-  environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = 1;
-  environment.sessionVariables.EDITOR = "nvim";
-  environment.sessionVariables.NIXOS_OZONE_WL = 1;
+  environment.sessionVariables = {
+    WLR_NO_HARDWARE_CURSORS = "1";
+    EDITOR = "nvim";
+    NIXOS_OZONE_WL = "1";
+  };
 
   programs.bash.promptInit = ''
     PS1="\n\[\033[1;32m\][\[\e]0;\u@\h:\w\a\]\w]$\[\033[0m\] "
@@ -101,48 +100,22 @@
     pydev = "nix-shell -E '(import /etc/nixos/python.nix {})' --command 'python3 -m venv .venv && source .venv/bin/activate && pip install -r requirements.txt'";
   };
 
-  security.pam.services.swaylock = { };
-
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
-    settings = {
-      General = {
-        Enable = "Source,Sink,Media,Socket";
-        AutoEnable = true;
-      };
-      Policy = {
-        AutoEnable = true;
-        ReconnectAttempts = 7;
-        ReconnectIntervals = "1,2,4,8,16,32,64";
-      };
-    };
   };
   services.blueman.enable = true;
 
   services.openssh.enable = false;
-  services.pipewire.enable = true;
-  services.pipewire.alsa.enable = true;
-  services.pipewire.pulse.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    pulse.enable = true;
+  };
   services.greetd.enable = true;
   services.greetd.settings.default_session.command = "${pkgs.sway}/bin/sway";
   services.greetd.settings.default_session.user = "chelsea";
   programs.ssh.startAgent = true;
-
-  # Enable nix-ld for pip install compatibility with compiled packages
-  programs.nix-ld = {
-    enable = true;
-    libraries = with pkgs; [
-      zlib zstd stdenv.cc.cc curl openssl attr libssh bzip2 libxml2 acl libsodium util-linux xz systemd
-      # Additional libraries commonly needed for Python packages
-      glib gtk3 cairo pango gdk-pixbuf atk
-      libffi ncurses readline sqlite
-      # Math/science libraries
-      blas lapack openblas
-      # Graphics libraries
-      freetype fontconfig
-    ];
-  };
 
   environment.systemPackages = with pkgs; [
     git
@@ -155,61 +128,31 @@
     nodejs
     awscli2
     aws-sam-cli
-
-    # Python with nix-ld compatibility
-    (writeShellScriptBin "python" ''
-      export LD_LIBRARY_PATH=$NIX_LD_LIBRARY_PATH
-      exec ${python3}/bin/python "$@"
-    '')
-    (writeShellScriptBin "python3" ''
-      export LD_LIBRARY_PATH=$NIX_LD_LIBRARY_PATH
-      exec ${python3}/bin/python3 "$@"
-    '')
-    (writeShellScriptBin "pip" ''
-      export LD_LIBRARY_PATH=$NIX_LD_LIBRARY_PATH
-      exec ${python3}/bin/pip "$@"
-    '')
-    (writeShellScriptBin "pip3" ''
-      export LD_LIBRARY_PATH=$NIX_LD_LIBRARY_PATH
-      exec ${python3}/bin/pip3 "$@"
-    '')
-    
-    python3Packages.pip
-    python3Packages.numpy
-    python3Packages.pandas
-    python3Packages.virtualenv
   ];
 
-  virtualisation.docker.enable = true;
-  virtualisation.docker.daemon.settings.experimental = true;
-  virtualisation.docker.daemon.settings.default-address-pools = [
-    {
-      base = "172.30.0.0/16";
-      size = 24;
-    }
-  ];
-  virtualisation.docker.rootless.enable = true;
-  virtualisation.docker.rootless.setSocketVariable = true;
+  virtualisation.docker = {
+    enable = true;
+    rootless = {
+      enable = true;
+      setSocketVariable = true;
+    };
+    daemon.settings = {
+      experimental = true;
+      default-address-pools = [{ base = "172.30.0.0/16"; size = 24; }];
+    };
+  };
 
-  users.mutableUsers = false;
-  users.allowNoPasswordLogin = true;
-  users.users.chelsea.isNormalUser = true;
-  users.users.chelsea.description = "chelsea";
-  users.users.chelsea.extraGroups = [
-    "networkmanager"
-    "wheel"
-    "docker"
-    "input"
-  ];
-  users.users.chelsea.initialPassword = "blah";
-  users.users.chelsea.packages = with pkgs; [
-    chromium
-    lazygit
-    zellij
-    qutebrowser
-    typescript
-    libreoffice
-  ];
+  users = {
+    mutableUsers = false;
+    allowNoPasswordLogin = true;
+    users.chelsea = {
+      isNormalUser = true;
+      description = "chelsea";
+      extraGroups = [ "networkmanager" "wheel" "docker" "input" ];
+      initialPassword = "blah";
+      packages = with pkgs; [ chromium lazygit zellij qutebrowser typescript libreoffice ];
+    };
+  };
   programs.nixvim = import "${nix-modules}/nixvim.nix" { inherit pkgs; };
 
   programs.chromium.extensions = [
@@ -219,13 +162,13 @@
     "mlomiejdfkolichcflejclcbmpeaniij" # Ghostery
   ];
 
-  home-manager.useGlobalPkgs = true;
-  home-manager.useUserPackages = true;
-  home-manager.backupFileExtension = "backup";
-
-  home-manager.users.chelsea =
-    { config, ... }:
-    {
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    backupFileExtension = "backup";
+    users.chelsea =
+      { config, ... }:
+      {
       home.username = "chelsea";
       home.homeDirectory = "/home/chelsea";
       home.stateVersion = "25.05";
@@ -297,28 +240,14 @@
 
       programs.waybar.enable = true;
       programs.waybar.systemd.enable = true;
-      programs.waybar.style = "
-      * {
-        font-size: 12px;
-        min-height: 0;
-				border-radius: 0;
-      }";
+      programs.waybar.style = "* { font-size: 12px; min-height: 0; border-radius: 0; }";
       programs.waybar.settings.mainBar = {
         layer = "top";
         position = "top";
         height = 18;
         modules-left = [ "sway/workspaces" ];
         modules-center = [ "sway/window" ];
-        modules-right = [
-          #"<volume>"
-          "cpu"
-          "temperature"
-          "memory"
-          "disk"
-          #"network"
-          #"battery"
-          "clock"
-        ];
+        modules-right = [ "cpu" "temperature" "memory" "disk" "clock" ];
         cpu.format = "| {usage}%";
         cpu.interval = 1;
 
@@ -361,24 +290,20 @@
       programs.qutebrowser.settings.content.javascript.clipboard = "access-paste";
 
       stylix.autoEnable = true;
-    };
+      };
+  };
+
+  fonts.packages = with pkgs; [ open-sans nerd-fonts.fira-code noto-fonts-emoji noto-fonts-cjk-sans ];
 
   stylix = {
     enable = true;
     image = ./wallpaper.png;
-
     base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-material-dark-medium.yaml";
-
-    fonts.serif.package = pkgs.open-sans;
-    fonts.serif.name = "Open Sans";
-    fonts.sansSerif.package = pkgs.open-sans;
-    fonts.sansSerif.name = "Open Sans";
-    fonts.monospace.package = pkgs.nerd-fonts.fira-code;
-    fonts.monospace.name = "Fira Code Nerdfont";
-    fonts.emoji.package = pkgs.noto-fonts-emoji;
-    fonts.emoji.name = "Noto Color Emoji";
+    fonts = {
+      serif = { package = pkgs.open-sans; name = "Open Sans"; };
+      sansSerif = { package = pkgs.open-sans; name = "Open Sans"; };
+      monospace = { package = pkgs.nerd-fonts.fira-code; name = "Fira Code Nerdfont"; };
+      emoji = { package = pkgs.noto-fonts-emoji; name = "Noto Color Emoji"; };
+    };
   };
-
-  # Enable Chinese
-  fonts.packages = with pkgs; [ noto-fonts-cjk-sans ];
 }
