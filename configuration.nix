9fb709e665ebf3e7bc8e978c1a57b623cf8f7552
Chelsea Wilkinson <mail@chelseawilkinson.me>
2025-10-11 15:41:54 +1100

    cachix

--- configuration.nix
+++ configuration.nix
@@ -64,6 +64,18 @@
     "flakes"
   ];
   
+  # Binary caches for faster builds
+  nix.settings.substituters = [
+    "https://cache.nixos.org"
+    "https://nix-community.cachix.org"
+    "https://nixpkgs-wayland.cachix.org"
+  ];
+  nix.settings.trusted-public-keys = [
+    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+  ];
+  
   # Automatic store optimization - hard-links identical files
   nix.settings.auto-optimise-store = true;
   nix.optimise.automatic = true;
