61708a16639f5d394c0d70090fdc11e7e810b4c1
Chelsea Wilkinson <mail@chelseawilkinson.me>
2026-02-01 15:50:40 +1100

    format

--- configuration.nix
+++ configuration.nix
@@ -16,7 +16,10 @@ in
   # ═══════════════════════════════════════════════════════════════════════════
   system.stateVersion = "25.05";
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
   nix.settings.substituters = [ "https://nix-community.cachix.org" ];
   nix.settings.trusted-public-keys = [
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
@@ -83,174 +86,201 @@ in
   users.allowNoPasswordLogin = true;
   users.users.chelsea.isNormalUser = true;
   users.users.chelsea.hashedPassword = "!";
-  users.users.chelsea.extraGroups = [ "networkmanager" "wheel" "docker" "input" ];
+  users.users.chelsea.extraGroups = [
+    "networkmanager"
+    "wheel"
+    "docker"
+    "input"
+  ];
 
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
-  home-manager.users.chelsea = { config, ... }: {
-    home.stateVersion = "25.05";
-    home.sessionVariables.EDITOR = "nvim";
-    home.sessionVariables.NIXOS_OZONE_WL = "1";
-    home.pointerCursor = {
-      gtk.enable = true;
-      package = pkgs.adwaita-icon-theme;
-      name = "Adwaita";
-      size = 16;
-    };
-    home.packages = with pkgs; [
-      age
-      chromium
-      lazygit
-      nodejs
-      shotman
-      sops
-      swaybg
-      wl-clipboard
-    ];
+  home-manager.users.chelsea =
+    { config, ... }:
+    {
+      home.stateVersion = "25.05";
+      home.sessionVariables.EDITOR = "nvim";
+      home.sessionVariables.NIXOS_OZONE_WL = "1";
+      home.pointerCursor = {
+        gtk.enable = true;
+        package = pkgs.adwaita-icon-theme;
+        name = "Adwaita";
+        size = 16;
+      };
+      home.packages = with pkgs; [
+        age
+        chromium
+        lazygit
+        nodejs
+        shotman
+        sops
+        swaybg
+        wl-clipboard
+      ];
 
-    # Secrets
-    sops.age.keyFile = "/home/chelsea/.config/sops/age/keys.txt";
-    sops.age.plugins = [ pkgs.age-plugin-yubikey ];
-    sops.defaultSopsFile = ./keys/secrets.yaml;
-    sops.secrets.github_token = { };
-    sops.secrets.anthropic_api_key = { };
+      # Secrets
+      sops.age.keyFile = "/home/chelsea/.config/sops/age/keys.txt";
+      sops.age.plugins = [ pkgs.age-plugin-yubikey ];
+      sops.defaultSopsFile = ./keys/secrets.yaml;
+      sops.secrets.github_token = { };
+      sops.secrets.anthropic_api_key = { };
 
-    # Window Manager
-    wayland.windowManager.sway = {
-      enable = true;
-      config = {
-        modifier = "Mod4";
-        terminal = "alacritty";
-        menu = "rofi -show run";
-        bars = [ ];
-        output."DP-1".mode = "3840x2160@180Hz";
-        output."DP-1".scale = "2";
-        window.titlebar = false;
-        gaps.smartGaps = true;
-        gaps.smartBorders = "no_gaps";
-        gaps.inner = 10;
-        gaps.outer = 10;
-        keybindings = lib.mkOptionDefault {
-          "Mod4+p" = "exec shotman --capture window";
-          "Mod4+Shift+p" = "exec shotman --capture region";
-          "Mod4+Ctrl+p" = "exec shotman --capture output";
+      # Window Manager
+      wayland.windowManager.sway = {
+        enable = true;
+        config = {
+          modifier = "Mod4";
+          terminal = "alacritty";
+          menu = "rofi -show run";
+          bars = [ ];
+          output."DP-1".mode = "3840x2160@180Hz";
+          output."DP-1".scale = "2";
+          window.titlebar = false;
+          gaps.smartGaps = true;
+          gaps.smartBorders = "no_gaps";
+          gaps.inner = 10;
+          gaps.outer = 10;
+          keybindings = lib.mkOptionDefault {
+            "Mod4+p" = "exec shotman --capture window";
+            "Mod4+Shift+p" = "exec shotman --capture region";
+            "Mod4+Ctrl+p" = "exec shotman --capture output";
+          };
         };
       };
-    };
 
-    # Programs
-    programs.alacritty.enable = true;
-    programs.alacritty.settings = {
-      cursor.style.shape = "Beam";
-      cursor.style.blinking = "On";
-      window.decorations = "buttonless";
-      window.padding.x = 14;
-      window.padding.y = 14;
-      font.size = lib.mkForce 10;
-    };
-
-    programs.bash = {
-      enable = true;
-      initExtra = ''
-        PS1="\n\[\033[1;32m\][\[\e]0;\u@\h:\w\a\]\w]$\[\033[0m\] "
-        export SOPS_AGE_KEY_FILE="$HOME/.config/sops/age/keys.txt"
-        [ -r "${config.sops.secrets.github_token.path}" ] && export GITHUB_TOKEN=$(cat ${config.sops.secrets.github_token.path})
-        [ -r "${config.sops.secrets.anthropic_api_key.path}" ] && export ANTHROPIC_API_KEY=$(cat ${config.sops.secrets.anthropic_api_key.path})
-      '';
-      shellAliases = {
-        edit = "sudo -E -s nvim";
-        Ec = "sudo -E -s nvim /etc/nixos/configuration.nix";
-        Ef = "sudo -E -s nvim /etc/nixos/flake.nix";
-        switch = "sudo nixos-rebuild switch";
-        nix-update = "cd /etc/nixos && sudo nix flake update";
-        nix-clean = "nix-env --delete-generations old --profile ~/.local/state/nix/profiles/home-manager && nix-collect-garbage -d && sudo nix-collect-garbage -d && sudo nix-store --optimise";
-        nix-verify = "sudo nix-store --verify --check-contents";
-        nix-full = "nix-update && switch && nix-clean && nix-verify";
-        pydev = "${
-          pkgs.buildFHSEnv {
-            name = "python-fhs";
-            targetPkgs = pkgs: with pkgs; [ python3 python3Packages.pip python3Packages.virtualenv ];
-            runScript = "bash";
-            profile = ''[ ! -f "requirements.txt" ] && return; virtualenv .venv; source .venv/bin/activate; pip install -q -r requirements.txt'';
-          }
-        }/bin/python-fhs";
+      # Programs
+      programs.alacritty.enable = true;
+      programs.alacritty.settings = {
+        cursor.style.shape = "Beam";
+        cursor.style.blinking = "On";
+        window.decorations = "buttonless";
+        window.padding.x = 14;
+        window.padding.y = 14;
+        font.size = lib.mkForce 10;
       };
-    };
 
-    programs.chromium.extensions = [
-      "mnjggcdmjocbbbhaepdhchncahnbgone"
-      "dbepggeogbaibhgnhhndojpepiihcmeb"
-      "gighmmpiobklfepjocnamgkkbiglidom"
-      "mlomiejdfkolichcflejclcbmpeaniij"
-    ];
-
-    programs.git.enable = true;
-    programs.git.settings = {
-      user.name = "Chelsea Wilkinson";
-      user.email = "mail@chelseawilkinson.me";
-      pull.rebase = true;
-      credential.helper = "store";
-    };
-
-    programs.qutebrowser = {
-      enable = true;
-      settings.tabs.show = "multiple";
-      settings.statusbar.show = "in-mode";
-      settings.content.javascript.clipboard = "access-paste";
-    };
-
-    programs.rofi.enable = true;
-    programs.rofi.extraConfig.hide-scrollbar = true;
-    programs.rofi.extraConfig.theme = lib.mkForce "gruvbox-dark-soft";
-
-    programs.ssh.enable = true;
-    programs.ssh.enableDefaultConfig = false;
-    programs.ssh.matchBlocks."*".serverAliveInterval = 60;
-    programs.ssh.matchBlocks."*".serverAliveCountMax = 3;
-
-    programs.swaylock.enable = true;
-
-    programs.vscode = {
-      enable = true;
-      package = pkgs.vscodium;
-      profiles.default.extensions = with pkgs.vscode-extensions; [ rooveterinaryinc.roo-cline ];
-      profiles.default.userSettings."roo-cline.anthropicApiKey" = "\${ANTHROPIC_API_KEY}";
-    };
-
-    programs.waybar = {
-      enable = true;
-      systemd.enable = true;
-      style = "* { font-size: 12px; min-height: 0; border-radius: 0; }";
-      settings.mainBar = {
-        height = 18;
-        modules-left = [ "sway/workspaces" ];
-        modules-center = [ "sway/window" ];
-        modules-right = [ "cpu" "temperature" "memory" "disk" "clock" ];
-        cpu.format = "| {usage}%";
-        cpu.interval = 1;
-        temperature.format = "({temperatureC}C)";
-        temperature.thermal-zone = 1;
-        temperature.interval = 1;
-        memory.format = "| {used}GiB ({percentage}%)";
-        memory.interval = 1;
-        disk.format = "| {used} ({percentage_used}%)";
-        disk.interval = 1;
-        clock.format = "| {:%a %d %b %I:%M:%S%p} |";
-        clock.interval = 1;
+      programs.bash = {
+        enable = true;
+        initExtra = ''
+          PS1="\n\[\033[1;32m\][\[\e]0;\u@\h:\w\a\]\w]$\[\033[0m\] "
+          export SOPS_AGE_KEY_FILE="$HOME/.config/sops/age/keys.txt"
+          [ -r "${config.sops.secrets.github_token.path}" ] && export GITHUB_TOKEN=$(cat ${config.sops.secrets.github_token.path})
+          [ -r "${config.sops.secrets.anthropic_api_key.path}" ] && export ANTHROPIC_API_KEY=$(cat ${config.sops.secrets.anthropic_api_key.path})
+        '';
+        shellAliases = {
+          edit = "sudo -E -s nvim";
+          Ec = "sudo -E -s nvim /etc/nixos/configuration.nix";
+          Ef = "sudo -E -s nvim /etc/nixos/flake.nix";
+          switch = "sudo nixos-rebuild switch";
+          nix-update = "cd /etc/nixos && sudo nix flake update";
+          nix-clean = "nix-env --delete-generations old --profile ~/.local/state/nix/profiles/home-manager && nix-collect-garbage -d && sudo nix-collect-garbage -d && sudo nix-store --optimise";
+          nix-verify = "sudo nix-store --verify --check-contents";
+          nix-full = "nix-update && switch && nix-clean && nix-verify";
+          pydev = "${
+            pkgs.buildFHSEnv {
+              name = "python-fhs";
+              targetPkgs =
+                pkgs: with pkgs; [
+                  python3
+                  python3Packages.pip
+                  python3Packages.virtualenv
+                ];
+              runScript = "bash";
+              profile = ''[ ! -f "requirements.txt" ] && return; virtualenv .venv; source .venv/bin/activate; pip install -q -r requirements.txt'';
+            }
+          }/bin/python-fhs";
+        };
       };
-    };
 
-    # Services
-    services.mako.enable = true;
-    services.swayidle.enable = true;
-    services.swayidle.timeouts = [
-      { timeout = 290; command = "${pkgs.libnotify}/bin/notify-send 'Locking in 10 seconds' -t 10000"; }
-      { timeout = 300; command = "${pkgs.systemd}/bin/systemctl suspend"; }
-    ];
-    services.swayidle.events = [
-      { event = "before-sleep"; command = "${pkgs.swaylock-effects}/bin/swaylock"; }
-    ];
-  };
+      programs.chromium.extensions = [
+        "mnjggcdmjocbbbhaepdhchncahnbgone"
+        "dbepggeogbaibhgnhhndojpepiihcmeb"
+        "gighmmpiobklfepjocnamgkkbiglidom"
+        "mlomiejdfkolichcflejclcbmpeaniij"
+      ];
+
+      programs.git.enable = true;
+      programs.git.settings = {
+        user.name = "Chelsea Wilkinson";
+        user.email = "mail@chelseawilkinson.me";
+        pull.rebase = true;
+        credential.helper = "store";
+      };
+
+      programs.qutebrowser = {
+        enable = true;
+        settings.tabs.show = "multiple";
+        settings.statusbar.show = "in-mode";
+        settings.content.javascript.clipboard = "access-paste";
+      };
+
+      programs.rofi.enable = true;
+      programs.rofi.extraConfig.hide-scrollbar = true;
+      programs.rofi.extraConfig.theme = lib.mkForce "gruvbox-dark-soft";
+
+      programs.ssh.enable = true;
+      programs.ssh.enableDefaultConfig = false;
+      programs.ssh.matchBlocks."*".serverAliveInterval = 60;
+      programs.ssh.matchBlocks."*".serverAliveCountMax = 3;
+
+      programs.swaylock.enable = true;
+
+      programs.vscode = {
+        enable = true;
+        package = pkgs.vscodium;
+        profiles.default.extensions = with pkgs.vscode-extensions; [ rooveterinaryinc.roo-cline ];
+        profiles.default.userSettings."roo-cline.anthropicApiKey" = "\${ANTHROPIC_API_KEY}";
+      };
+
+      programs.waybar = {
+        enable = true;
+        systemd.enable = true;
+        style = "* { font-size: 12px; min-height: 0; border-radius: 0; }";
+        settings.mainBar = {
+          height = 18;
+          modules-left = [ "sway/workspaces" ];
+          modules-center = [ "sway/window" ];
+          modules-right = [
+            "cpu"
+            "temperature"
+            "memory"
+            "disk"
+            "clock"
+          ];
+          cpu.format = "| {usage}%";
+          cpu.interval = 1;
+          temperature.format = "({temperatureC}C)";
+          temperature.thermal-zone = 1;
+          temperature.interval = 1;
+          memory.format = "| {used}GiB ({percentage}%)";
+          memory.interval = 1;
+          disk.format = "| {used} ({percentage_used}%)";
+          disk.interval = 1;
+          clock.format = "| {:%a %d %b %I:%M:%S%p} |";
+          clock.interval = 1;
+        };
+      };
+
+      # Services
+      services.mako.enable = true;
+      services.swayidle.enable = true;
+      services.swayidle.timeouts = [
+        {
+          timeout = 290;
+          command = "${pkgs.libnotify}/bin/notify-send 'Locking in 10 seconds' -t 10000";
+        }
+        {
+          timeout = 300;
+          command = "${pkgs.systemd}/bin/systemctl suspend";
+        }
+      ];
+      services.swayidle.events = [
+        {
+          event = "before-sleep";
+          command = "${pkgs.swaylock-effects}/bin/swaylock";
+        }
+      ];
+    };
 
   # ═══════════════════════════════════════════════════════════════════════════
   # THEMING
@@ -263,3 +293,4 @@ in
   stylix.fonts.monospace = mkFont pkgs.nerd-fonts.fira-code "Fira Code Nerdfont";
   stylix.fonts.emoji = mkFont pkgs.noto-fonts-color-emoji "Noto Color Emoji";
 }
+
